d53918ec2f605b435708568a77bf2a8953c3beb9
IMRE Lab <um.imre@maine.edu>
2018-10-31 16:24:37 -0400

    changd to rpc only float rather than whole color

--- Assets/Scripts/HyperBall.cs
+++ Assets/Scripts/HyperBall.cs
@@ -24,9 +24,8 @@ namespace IMRE.HandWaver.FourthDimension {
 
 		private void Start()
 		{
-			Color tmp = UnityEngine.Random.ColorHSV(0, 1, 1, 1, 1, 1);
-			Vector4 tmpVec = new Vector4(tmp.r, tmp.g, tmp.b, tmp.a);
-			photonView.RPC("setColorOnBall", RpcTarget.All, tmpVec);
+			float tmp = UnityEngine.Random.Range(0, 1);
+			photonView.RPC("setColorOnBall", RpcTarget.All, tmp);
 
 			GetComponent<InteractionBehaviour>().OnContactBegin += startTakeOver;
 			GetComponent<InteractionBehaviour>().OnContactEnd += endTakeOver;
@@ -37,9 +36,10 @@ namespace IMRE.HandWaver.FourthDimension {
 		/// </summary>
 		/// <param name="tmpColor">color to set for this ball</param>
 		[PunRPC]
-		void setColorOnBall(Vector4 tmpColor)
+		void setColorOnBall(float hue)
 		{
-			this.GetComponent<MeshRenderer>().materials[0].color = new Color(tmpColor.x, tmpColor.y, tmpColor.z, tmpColor.w);
+			this.GetComponent<MeshRenderer>().materials[0].color = UnityEngine.Random.ColorHSV(hue, hue, 1, 1, 1, 1);
+
 		}
 
 		void Update() {
