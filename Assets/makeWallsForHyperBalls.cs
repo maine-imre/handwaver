948e123a3b02a8c7d5c69080b566074a27d6e084
IMRE Lab <um.imre@maine.edu>
2018-11-01 16:04:47 -0400

    wall positions

--- Assets/makeWallsForHyperBalls.cs
+++ Assets/makeWallsForHyperBalls.cs
@@ -18,12 +18,12 @@ namespace IMRE.HandWaver.FourthDimension
 		// Use this for initialization
 		void Start()
 		{
-			GameObject.Instantiate(wallPrefab, Vector3.forward * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.forward), this.transform).localScale = Vector3.one * 0.1f * scaleOfBox / 2f;
-			GameObject.Instantiate(wallPrefab, Vector3.back * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.back), this.transform).localScale = Vector3.one * 0.1f * scaleOfBox / 2f;
-			GameObject.Instantiate(wallPrefab, Vector3.up * 2 * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.up), this.transform).localScale = Vector3.one * 0.1f * scaleOfBox / 2f;
-			GameObject.Instantiate(wallPrefab, Vector3.down * 0 * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.down), this.transform).localScale = Vector3.one * 0.1f * scaleOfBox / 2f;
-			GameObject.Instantiate(wallPrefab, Vector3.right * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.right), this.transform).localScale = Vector3.one * 0.1f * scaleOfBox / 2f;
-			GameObject.Instantiate(wallPrefab, Vector3.left * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.left), this.transform).localScale = Vector3.one * 0.1f * scaleOfBox / 2f;
+			GameObject.Instantiate(wallPrefab, Vector3.forward * scaleOfBox+Vector3.up*scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.forward), this.transform).localScale = Vector3.one * scaleOfBox / 2f;
+			GameObject.Instantiate(wallPrefab, Vector3.back * scaleOfBox + Vector3.up * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.back), this.transform).localScale = Vector3.one *  scaleOfBox / 2f;
+			GameObject.Instantiate(wallPrefab, Vector3.up * 2 * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.up), this.transform).localScale = Vector3.one *  scaleOfBox / 2f;
+			GameObject.Instantiate(wallPrefab, Vector3.down * 0 * scaleOfBox+Vector3.up*scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.down), this.transform).localScale = Vector3.one *  scaleOfBox / 2f;
+			GameObject.Instantiate(wallPrefab, Vector3.right * scaleOfBox + Vector3.up * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.right), this.transform).localScale = Vector3.one *  scaleOfBox / 2f;
+			GameObject.Instantiate(wallPrefab, Vector3.left * scaleOfBox + Vector3.up * scaleOfBox, Quaternion.FromToRotation(Vector3.up, Vector3.left), this.transform).localScale = Vector3.one *  scaleOfBox / 2f;
 		}
 	}
 }
