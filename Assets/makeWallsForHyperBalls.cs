27558b1b1b348183ac5af23f8e00d548b102b1ff
IMRE Lab <um.imre@maine.edu>
2018-11-01 17:07:27 -0400

    fix one wall...

--- Assets/makeWallsForHyperBalls.cs
+++ Assets/makeWallsForHyperBalls.cs
@@ -21,7 +21,7 @@ namespace IMRE.HandWaver.FourthDimension
 			this.transform.position = Vector3.zero;
 
 			GameObject.Instantiate(wallPrefab, Vector3.forward * scaleOfBox+HyperBall.origin, Quaternion.FromToRotation(Vector3.up, Vector3.forward), this.transform).localScale = Vector3.one * scaleOfBox / 5f;
-			GameObject.Instantiate(wallPrefab, Vector3.back * scaleOfBox +HyperBall.origin + HyperBall.origin, Quaternion.FromToRotation(Vector3.up, Vector3.back), this.transform).localScale = Vector3.one *  scaleOfBox / 5f;
+			GameObject.Instantiate(wallPrefab, Vector3.back * scaleOfBox +HyperBall.origin, Quaternion.FromToRotation(Vector3.up, Vector3.back), this.transform).localScale = Vector3.one *  scaleOfBox / 5f;
 			GameObject.Instantiate(wallPrefab, Vector3.up * scaleOfBox + HyperBall.origin, Quaternion.FromToRotation(Vector3.up, Vector3.up), this.transform).localScale = Vector3.one *  scaleOfBox / 5f;
 			GameObject.Instantiate(wallPrefab, Vector3.down * scaleOfBox + HyperBall.origin, Quaternion.FromToRotation(Vector3.up, Vector3.down), this.transform).localScale = Vector3.one *  scaleOfBox / 5f;
 			GameObject.Instantiate(wallPrefab, Vector3.right * scaleOfBox + HyperBall.origin, Quaternion.FromToRotation(Vector3.up, Vector3.right), this.transform).localScale = Vector3.one *  scaleOfBox / 5f;
