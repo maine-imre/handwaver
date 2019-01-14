22aab670c644a47e4e0cd3414b0445198fc28bf2
Camden Bock <cam3715@users.noreply.github.com>
2019-01-14 16:10:51 -0500

    Create SwipeGesture.cs

---
+++ Assets/Scripts/gestures/GestureDefinitions/SwipeGesture.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR
+
+namespace IMRE.Gestures
+{
+public class SwipeGesture : OneHandedGesture {
+
+}
+}
