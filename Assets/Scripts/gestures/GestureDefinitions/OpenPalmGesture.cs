3591ca6cb59c2502c001703259b4c5dc7636d734
Camden Bock <cam3715@users.noreply.github.com>
2019-01-14 16:10:18 -0500

    Create OpenPalmGesture.cs

---
+++ Assets/Scripts/gestures/GestureDefinitions/OpenPalmGesture.cs
@@ -0,0 +1,13 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR
+
+namespace IMRE.Gestures
+{
+public class OpenPalmGesture : OneHandedGesture {
+
+}
+}
