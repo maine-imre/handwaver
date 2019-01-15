63c40fd2bd8ea96cdc5c003736e24015fc140d29
Camden Bock <cam3715@users.noreply.github.com>
2019-01-15 11:14:12 -0500

    Update RevolveGesture.cs

--- Assets/Scripts/gestures/GestureImplementations/RevolveGesture.cs
+++ Assets/Scripts/gestures/GestureImplementations/RevolveGesture.cs
@@ -6,7 +6,13 @@ using UnityEngine.XR;
 
 namespace IMRE.Gestures
 {
-public class RevolveGesture : SwipeGesture {
+  public class RevolveGesture : SwipeGesture {
+    public bool DeactivationConditionsActionComplete(){
+      return false;
+    }
 
-}
+    public bool WhileGestureActive(Hand hand,InputDevice osvrController){
+      return false;
+    }
+  }
 }
