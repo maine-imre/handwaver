fd035345ca2e41027c6ca3a8faeedc793a234fe3
Camden Bock <cam3715@users.noreply.github.com>
2019-01-15 11:14:26 -0500

    Update RotateEarthGesture.cs

--- Assets/Scripts/gestures/GestureImplementations/RotateEarthGesture.cs
+++ Assets/Scripts/gestures/GestureImplementations/RotateEarthGesture.cs
@@ -6,7 +6,13 @@ using UnityEngine.XR;
 
 namespace IMRE.Gestures
 {
-public class RotateEarthGesture : SwipeGesture {
+  public class RotateEarthGesture : SwipeGesture {
+    public bool DeactivationConditionsActionComplete(){
+      return false;
+    }
 
-}
+    public bool WhileGestureActive(Hand hand,InputDevice osvrController){
+      return false;
+    }
+  }
 }
