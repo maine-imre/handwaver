85fd5a7b4601c80c4960de77a3c343031aaedbb7
Camden Bock <cam3715@users.noreply.github.com>
2019-01-15 11:09:15 -0500

    Update DoublePinchNoGraspGesture.cs

--- Assets/Scripts/gestures/GestureDefinitions/DoublePinchNoGraspGesture.cs
+++ Assets/Scripts/gestures/GestureDefinitions/DoublePinchNoGraspGesture.cs
@@ -6,7 +6,41 @@ using UnityEngine.XR
 
 namespace IMRE.Gestures
 {
-public class DoublePinchNoGraspGesture : OneHandedGesture {
+public abstract class DoublePinchNoGraspGesture : OneHandedGesture {
+
+    internal void visualFeedbackActivated(){
+    }
+    internal void tactileFeedbackActivated(){
+    }
+    //we need to require an audioPlayer component.
+    internal void audioFeedbackActivated(){
+    }
+
+    internal void visualFeedbackDeactivated(DeactivationReason reason){
+    }
+    internal void tactileFeedbackDeactivated(DeactivationReason reason){
+    }
+      //we need to require an audioPlayer component.
+    internal void audioFeedbackDeactivated(DeactivationReason reason){
+    }
+
+    public  bool ActivationConditionsHand(Hand hand);
+    {
+      return false;
+    }
+    public  bool ActivationConditionsOSVR(InputDevice osvrController);
+    {
+      return false;
+    }
+    public  bool DeactivationConditionsHand(Hand hand);
+    {
+      return false;
+    }
+    public  bool DeactivationConditionsOSVR(InputDevice osvrController);
+    {
+      return false;
+    }
+
 
 }
 }
