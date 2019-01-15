aac67672399c38b800b46d79ed73f3268a207b1f
Camden Bock <cam3715@users.noreply.github.com>
2019-01-15 11:09:52 -0500

    Update PointAtGesture.cs

--- Assets/Scripts/gestures/GestureDefinitions/PointAtGesture.cs
+++ Assets/Scripts/gestures/GestureDefinitions/PointAtGesture.cs
@@ -8,5 +8,37 @@ namespace IMRE.Gestures
 {
 public class PointAtGesture : OneHandedGesture {
 
+    internal void visualFeedbackActivated(){
+    }
+    internal void tactileFeedbackActivated(){
+    }
+    //we need to require an audioPlayer component.
+    internal void audioFeedbackActivated(){
+    }
+
+    internal void visualFeedbackDeactivated(DeactivationReason reason){
+    }
+    internal void tactileFeedbackDeactivated(DeactivationReason reason){
+    }
+      //we need to require an audioPlayer component.
+    internal void audioFeedbackDeactivated(DeactivationReason reason){
+    }
+
+    public  bool ActivationConditionsHand(Hand hand);
+    {
+      return false;
+    }
+    public  bool ActivationConditionsOSVR(InputDevice osvrController);
+    {
+      return false;
+    }
+    public  bool DeactivationConditionsHand(Hand hand);
+    {
+      return false;
+    }
+    public  bool DeactivationConditionsOSVR(InputDevice osvrController);
+    {
+      return false;
+    }
 }
 }
