52aa536e7a3bef7bbd2c213b808341594a877f61
Camden Bock <cam3715@users.noreply.github.com>
2019-01-15 11:13:06 -0500

    Update LatticeLandEraseGesture.cs

--- Assets/Scripts/gestures/GestureImplementations/LatticeLandEraseGesture.cs
+++ Assets/Scripts/gestures/GestureImplementations/LatticeLandEraseGesture.cs
@@ -6,7 +6,13 @@ using UnityEngine.XR;
 
 namespace IMRE.Gestures
 {
-public class LatticeLandEraseGesture : SwipeGesture {
+  public class LatticeLandEraseGesture : SwipeGesture {
+    public bool DeactivationConditionsActionComplete(){
+      return false;
+    }
 
-}
+    public bool WhileGestureActive(Hand hand,InputDevice osvrController){
+      return false;
+    }
+  }
 }
