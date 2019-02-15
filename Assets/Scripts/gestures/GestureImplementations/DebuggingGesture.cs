e109767bc1850bd3aedd93e37fcae94bd9b78319
Joey Haney <joseph.e.haney@maine.edu>
2019-02-15 16:46:07 -0500

    Added the gameObject called Debugging and the script for debugging gestures before a proper implementation of the gestures is made in order to test their functionality as they are migrated to the new system.
    ** Will have to delete the object debugging within
    Handwaver base when testing is done

---
+++ Assets/Scripts/gestures/GestureImplementations/DebuggingGesture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Leap;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace IMRE.Gestures
+{
+	public class DebuggingGesture : PointAtGesture
+	{
+		protected override bool DeactivationConditionsActionComplete()
+		{
+			return false;
+		}
+
+		protected override void WhileGestureActive(Leap.Hand hand, InputDevice osvrController)
+		{
+			Debug.Log("GESTURE ACTIVE");
+		}
+	}
+}
