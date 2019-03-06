00fd78e95fab40fd1d22d0aacfa4057ac2e5848c
Joey Haney <joseph.e.haney@maine.edu>
2019-03-06 16:57:01 -0500

    It needs to be gone over and properly implemented soon, for now this is the current state

--- Assets/Scripts/gestures/GestureImplementations/SelectGesture.cs
+++ Assets/Scripts/gestures/GestureImplementations/SelectGesture.cs
@@ -16,7 +16,47 @@ namespace IMRE.Gestures
 
 		protected override void WhileGestureActive(Leap.Hand hand, InputDevice osvrController)
 		{
-			throw new NotImplementedException();
-		}
+            float shortestDist = Mathf.Infinity;
+            closestObj = null;
+
+            foreach (MasterGeoObj mgo in FindObjectsOfType<MasterGeoObj>().Where(g => (g.GetComponent<AnchorableBehaviour>() == null || (g.GetComponent<AnchorableBehaviour>() != null && !g.GetComponent<AnchorableBehaviour>().isAttached))))
+            {
+                float distance = mgo.LocalDistanceToClosestPoint(hand.Fingers[1].TipPosition.ToVector3());
+                float angle = mgo.PointingAngleDiff(hand.Fingers[1].TipPosition.ToVector3(), hand.Fingers[1].Direction.ToVector3());
+
+                if (Mathf.Abs(distance) < shortestDist)
+                {
+                    if (distance < shortestDist && angle < angleTolerance)
+                    {
+                        closestObj = mgo;
+                        shortestDist = distance;
+                    }
+                }
+                else
+                {
+                    //check to see if any higher priority objectes lie within epsilon
+                    bool v = (Mathf.Abs(distance) - shortestDist <= maximumRangeToSelect) && (
+                      ((closestObj.figType == GeoObjType.line || closestObj.figType == GeoObjType.polygon) && mgo.figType == GeoObjType.point)
+                      || (closestObj.figType == GeoObjType.polygon && mgo.figType == GeoObjType.point)
+                      );
+                    if (v)
+                    {
+                        closestObj = mgo;
+                        shortestDist = distance;
+                    }
+                }
+            }
+
+            if (closestObj != null && shortestDist <= maximumRangeToSelect)
+            {
+                if (debugSelect)
+                    Debug.Log(closestObj + " is the object toggling selection state.");
+
+                if (closestObj.IsSelected)
+                    closestObj.thisSelectStatus = MasterGeoObj.SelectionStatus.none;
+                else
+                    closestObj.thisSelectStatus = MasterGeoObj.SelectionStatus.selected;
+                completeBool = true;
+            }
 	}
 }
