16bf987398359380e5a35a43563c6dc08500c495
Camden Bock <camden.bock@maine.edu>
2019-05-06 18:47:06 -0400

    Update the PUN RPCs to allow for different models depending on the choice of controller type.
    Further generalization of controller type.  Note this is a placeholder for a future gesture system that isn't based on LM.  Also, we will consider using Node for our networking solution in the future.
    
    Additionally, this moves the syncing of model state to act on an IEnumerator rather than on update.  A small optimization to allow for smoother movement of objects.
    
    Consider what framerate is optimal for updating the objects movement.  We could update the objects on the network at 30 fps while rendering at 90 fps.  this would move the transform copies out of update and into their own coroutine.
    
    Needs testing.

--- Assets/Scripts/Networking/playerHand.cs
+++ Assets/Scripts/Networking/playerHand.cs
@@ -9,38 +9,82 @@ using System.Linq;
 
 namespace IMRE.HandWaver.Networking
 {
-/// <summary>
-/// Syncs visual representations of players' hands.
-/// Will be expanded to handle InterationHands and Interaction Controllers.
-/// </summary>
+	/// <summary>
+	/// Syncs visual representations of players' hands.
+	/// Will be expanded to handle InterationHands and Interaction Controllers.
+	/// </summary>
 	public class playerHand : MonoBehaviourPunCallbacks
 	{
 		public Chirality whichHand;
-
-		public InteractionHand iHand;
-		public InteractionController iController;
-
+		
 		public Mesh handModel;
 		public Mesh controllerModel;
+
+		public int LeapStatusCheckInterval = 1f;
+
+		internal bool isLeapMotionActive
+		{
+			//check to see if a leap motion device is present
+			get
+			{
+				//thankfully leap motion has a function for this.
+				current = FindObjectOfType<LeapServiceProvider>().IsConnected();
+				//to double check, see if an XR controller exists.  This should always be true.
+				if (!FindObjectOfType<LeapXRServiceProvider>().IsConnected() && !current)
+				{
+					Debug.LogWarning("No HandTracking is Available!  Please attach a controller.");
+				}
+			}
+		}
+
+		private bool wasLeapMotionActive;
+
+		internal InteractionHand iHand
+		{
+			get
+			{
+				if (_iHand == null)
+				{
+					//find the object in the scene with the appropriate chirality.
+					_iHand = FindObjectsOfType<InteractionHand>().Where(h => (h.handDataMode == HandDataMode.PlayerLeft) == (whichHand = Chirality.Left)).FirstOrDefault();
+				}
+
+				return _iHand;
+			}
+		}
+
+		private InteractionHand _iHand;
+
+
+		internal InteractionXRController iController
+		{
+			get
+			{
+				if (_iXR == null)
+				{
+					//find the object in the scene with the appropriate chirality.
+					_iXR = FindObjectsOfType<InteractionXRController>().Where(h => (h.handDataMode == HandDataMode.PlayerLeft) == (whichHand = Chirality.Left)).FirstOrDefault();
+				}
+			}
+		}
+
+		private InteractionXRController _iXR;
+
+		private InteractionController iController
+		{
+			get
+			{
+				//chose the object in the scene that is active.  Prefer LeapMotion.
+				return isLeapMotionActive ? iHand : iXR;
+			}
+		}
+		
+		private IEnumerator leapStatus = checkLeapStatus(LeapStatusCheckInterval);
 
 		private void Start()
 		{
 			if (photonView.IsMine)
 			{
-				//Sets hand based on which hand is needed from whichHand variable.
-				switch (whichHand)
-				{
-					case Chirality.Left:
-						iHand = FindObjectsOfType<InteractionHand>().Where(h => h.handDataMode == HandDataMode.PlayerLeft).FirstOrDefault();
-						break;
-					case Chirality.Right:
-						iHand = FindObjectsOfType<InteractionHand>().Where(h => (h.handDataMode == HandDataMode.PlayerRight)).FirstOrDefault();
-						break;
-					default:
-						Debug.LogError(name + " has encountered an error with whichHand variable.");
-						break;
-				}
-
 				//Asserts that the hand has found something and been assigned.
 				if(iHand == null)
 				{
@@ -50,16 +94,29 @@ namespace IMRE.HandWaver.Networking
 				{
 					Debug.Log("Local Player " + whichHand + " was set to be interaction hand" + iHand.name);
 				}
+				//turn off the renderer locally.
 				GetComponent<MeshRenderer>().enabled = false;
-			}
+				
+				//sync the "showHand variable".  We don't want to sync to self becasue we don't want to see the approximation mesh.
+				photonView.RPC("showHand", RpcTarget.OthersBuffered, iController.isTracked);
+				
+				//initial LM check and sync to others.  We don't want to sync to self because we don't care what controller type 
+				//is used for a mesh we don't see.
+				photonView.RPC("handMode", RpcTarget.OthersBuffered, isLeapMotionActive);
 
-			if (iHand != null)
-			{
-				handMode(true);
+				//start a co-routine to allow LM to be plugged or unpluged at regular intervals.
+				//this will run the above commands as necessary.
+				StartCoroutine(leapStatus);
 			}
-			else if (iController !- null)
+		}
+		
+		private void Update()
+		{
+			if (photonView.IsMine)
 			{
-				handMode(false);
+				//update the position and rotation of the controller.  PUN handels syncing this data.
+				transform.position = iController.position;
+				transform.rotation = iController.transform.rotation;
 			}
 		}
 
@@ -74,15 +131,21 @@ namespace IMRE.HandWaver.Networking
 		{
 			GetComponent<MeshRenderer>().mesh = isHand ? handModel : controllerModel;
 		}
-		
-		private void Update()
-		{
-			if (photonView.IsMine)
-			{
-				transform.position = iHand.position;
 
-				photonView.RPC("showHand", RpcTarget.OthersBuffered, iHand.isTracked);
+		private IEnumerator checkLeapStatus(int waitTime)
+		{
+			while (true)
+			{
+				//sync the "showHand variable".  We don't want to sync to self becasue we don't want to see the approximation mesh.
+				photonView.RPC("showHand", RpcTarget.OthersBuffered, iController.isTracked);
+				
+				//initial LM check and sync to others.  We don't want to sync to self because we don't care what controller type 
+				//is used for a mesh we don't see.
+				photonView.RPC("handMode", RpcTarget.OthersBuffered, isLeapMotionActive);
+				yield return new WaitForSeconds(waitTime);
 			}
 		}
+		
+		
 	}
 }
