201204519d7d12fa33759a4469f2d17aafd0c7f3
Nathan Gazey <ngazey60@maine.edu>
2019-05-21 11:26:28 -0400

    Added a base scene for this. basic idea is present. Part way through making the manager

---
+++ Assets/Scripts/SceneSpecific/variedPerspective/perspectiveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IMRE.HandWaver.VariedPerspective
+{
+
+    public class perspectiveManager : MonoBehaviour
+    {
+        
+        public enum perspectiveMode
+        {
+            None, 
+            OnlyMain, 
+            OnlyOrtho
+        }
+        
+        private static Camera _mainCamera = Camera.main;
+        public Camera orthoCamera;
+        public perspectiveMode _currentMode;
+
+        public perspectiveMode currentMode
+        {
+            get => _currentMode;
+            set
+            {
+                _currentMode = value;
+                
+            }
+        }
+        
+        
+
+
+    }
+}
