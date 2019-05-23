1ac30e4435c0b514f48b33984a84e9915ced5900
Nathan Gazey <ngazey60@maine.edu>
2019-05-23 12:35:04 -0400

    Allowed the  observer to switch the mode with f10-f12 to change which cameras view what. This is a completion of the described scene from camden.

--- Assets/Scripts/SceneSpecific/variedPerspective/perspectiveManager.cs
+++ Assets/Scripts/SceneSpecific/variedPerspective/perspectiveManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +8,36 @@ namespace IMRE.HandWaver.VariedPerspective
 
     public class perspectiveManager : MonoBehaviour
     {
-        
+
+        /// <summary>
+        /// Enumerator that describes the three modes to be used for this scene.
+        /// </summary>
         public enum perspectiveMode
         {
-            None, 
-            OnlyMain, 
+            None,
+            OnlyMain,
             OnlyOrtho
         }
-        
-        private static Camera _mainCamera = Camera.main;
+
+        /// <summary>
+        /// The VR View Camera.
+        /// </summary>
+        private static Camera _mainCamera;
+        /// <summary>
+        /// The list of all main scene cameras. This will include potentially mixcast cameras.
+        /// </summary>
+        public List<Camera> mainCameras;
+        /// <summary>
+        /// The orthogonal camera used to get a flat perspective on the shapes within the scene.
+        /// </summary>
         public Camera orthoCamera;
+        /// <summary>
+        /// This is just a public version of the perspective mode for debug view within the editor.
+        /// Editing this has no effect on the actual mode.
+        /// </summary>
         public perspectiveMode _currentMode;
+
+        private int shapeLayer = -1;
 
         public perspectiveMode currentMode
         {
@@ -25,12 +45,77 @@ namespace IMRE.HandWaver.VariedPerspective
             set
             {
                 _currentMode = value;
-                
+                switch (value)
+                {
+                    case perspectiveMode.None:
+                        // Hide the shape layer from all cameras
+                        
+                        mainCameras.ForEach(mc=> mc.cullingMask &= ~shapeLayer);
+                        orthoCamera.cullingMask &= ~shapeLayer;
+                        break;
+                    case perspectiveMode.OnlyMain:
+                        // Hide the shape layer from the ortho camera
+                        
+                        mainCameras.ForEach(mc=> mc.cullingMask &= ~shapeLayer);
+                        orthoCamera.cullingMask |= shapeLayer;
+
+                        break;
+                    case perspectiveMode.OnlyOrtho:
+                        // Hide the shape layer from the main cameras
+                        
+                        mainCameras.ForEach(mc=> mc.cullingMask |= shapeLayer);
+                        
+                        orthoCamera.cullingMask &= ~shapeLayer;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
             }
         }
-        
-        
+
+        /// <summary>
+        /// Set the perspective mode to none.
+        /// This means that no camera sees the shape layer.
+        /// </summary>
+        private void SetNone() => currentMode = perspectiveMode.None;
+        /// <summary>
+        ///  Set the perspective mode to only show shapes on main cameras.
+        /// This disallows the view of projection and allows the view of the source objects.
+        /// </summary>
+        private void SetOnlyOrtho() => currentMode = perspectiveMode.OnlyMain;
+        /// <summary>
+        /// Set the perspective mode to only show shapes on orthogonal camera.
+        /// This will be effectively only showing the projection and not the source object.
+        /// </summary>
+        private void SetOnlyMain() => currentMode = perspectiveMode.OnlyOrtho;
+
+        private void Awake()
+        {
+            // find reference to main camera
+            _mainCamera = Camera.main;
+            // Add the VR view into the main cameras list
+            mainCameras.Add(_mainCamera);
+            // Find the Shape Layer
+            shapeLayer = 1 << LayerMask.NameToLayer("ShapeLayer");
+            
+            if (shapeLayer == -1)    //If shape layer is not found
+            {
+                Debug.LogError("Shape Layer not found!");
+            }
+            
+        }
 
 
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.F10))
+                SetNone();
+            if(Input.GetKeyDown(KeyCode.F11))
+                SetOnlyMain();
+            if(Input.GetKeyDown(KeyCode.F12))
+                SetOnlyOrtho();
+            
+        }
     }
 }
