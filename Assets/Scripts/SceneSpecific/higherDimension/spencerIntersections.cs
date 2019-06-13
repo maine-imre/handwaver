3ab400661ce2df7c34abf82acb0c9fdbec677068
Camden Bock <camden.bock@maine.edu>
2019-06-13 11:10:22 -0400

    Update spencerIntersections.cs

--- Assets/Scripts/SceneSpecific/higherDimension/spencerIntersections.cs
+++ Assets/Scripts/SceneSpecific/higherDimension/spencerIntersections.cs
@@ -10,7 +10,19 @@ public class intersectionScript : MonoBehaviour
     public float crossSectionHeight = .5f;
 
     void Start()
+    {
+ //TODO @Camden render the basic figures
+    //TODO render circle
+    //TODO render annulus
+    //TODO render sphere
+    //TODO render torus
+    //TODO render hypersphere
+    //TODO render hypertorus
+    }
+    
+    void Update()
     {   
+//TODO @Camden make these functions dynamically accept the paramters from the figures
         crossSectCirc(1f,crossSectionHeight);
         crossSectAnnulus(.5f,1f,crossSectAnnulus);
         crossSectSphere(1f,Vector3.zero,crossSectionHeight,Vector3.up);
@@ -26,6 +38,7 @@ public class intersectionScript : MonoBehaviour
         
         if (height == radius)
         {
+//TODO @Rene calculate figure parameters
                         //comment here to explain what this condition case is geometerically
 
             //the positing of the point is a Vector3.  
@@ -35,9 +48,12 @@ public class intersectionScript : MonoBehaviour
             //suggest this:
             //Vector3 pointPos = Vector3.up*radius;
             //Debug.Log(pointPos);
+//TODO @Camden Render Figure
         }
         //else if (-height == radius)
         //{
+//TODO @Rene calculate figure parameters
+
                                     //comment here to explain what this condition case is geometerically
 
             //the positing of the point is a Vector3.  
@@ -45,9 +61,13 @@ public class intersectionScript : MonoBehaviour
             
             //Vector3 pointPos = Vector3.down*radius;
             //Debug.Log(pointPos);
+ //TODO @Camden Render Figure
+
        // }
         else if (height < radius)
         {
+ //TODO @Rene calculate figure parameters
+
                         //comment here to explain what this condition case is geometerically
 
             //if the height is smaller than the inner radius, then each line segment is defined by 2 Vector3s (one for each endpoint).
@@ -60,6 +80,9 @@ public class intersectionScript : MonoBehaviour
             //Vector3 SegmentAEndpoint0 = 
             //Vector3 SegmentAEndpoint1 = 
             //Debug.Log(SegmentAEndpoint0, SegmentAEndpoint1);
+            
+ //TODO @Camden Render Figure
+
          }
          //what if height > radius
 
@@ -67,6 +90,7 @@ public class intersectionScript : MonoBehaviour
 
     public void crossSectAnnulus(float innerRadius, float outerRadius, float height)
     {
+
         //you might think about this as two different circle intersection problems, and use bits of your previous function
     
         //assuming annulus is centered at zero.
@@ -77,6 +101,8 @@ public class intersectionScript : MonoBehaviour
 
         if (height == outerRadius)
         {
+ //TODO @Rene calculate figure parameters
+
                         //comment here to explain what this condition case is geometerically
 
             //the positing of the point is a Vector3.  
@@ -86,9 +112,13 @@ public class intersectionScript : MonoBehaviour
             //suggest this:
             //Vector3 pointPos = 
             //Debug.Log(pointPos);
+//TODO @Camden Render Figure
+
         }
         else if (height < innerRadius )
         {
+ //TODO @Rene calculate figure parameters
+
                         //comment here to explain what this condition case is geometerically
 
             //if the height is smaller than the inner radius, then each line segment is defined by 2 Vector3s (one for each endpoint).
@@ -102,10 +132,13 @@ public class intersectionScript : MonoBehaviour
             //Vector3 SegmentBEndpoint0 = 
             //Vector3 SegmentBEndpoint1 = 
             //Debug.Log(SegmentAEndpoint0, SegmentAEndpoint1, SegmentBEndpoint0, SegmentBEndpoint1);
+//TODO @Camden Render Figure
 
         }
         else if (height > innerRadius && height < outerRadius)
         {
+  //TODO @Rene calculate figure parameters
+
                 //comment here to explain what this condition case is geometerically
 
             //if the height is smaller than the inner radius, then each line segment is defined by 2 Vector3s (one for each endpoint).
@@ -118,7 +151,8 @@ public class intersectionScript : MonoBehaviour
             //Vector3 SegmentAEndpoint0 = 
             //Vector3 SegmentAEndpoint1 = 
             //Debug.Log(SegmentAEndpoint0, SegmentAEndpoint1);
-            
+ //TODO @Camden Render Figure
+           
         }
         //here you've missed the case where height == innerRadius.
 
@@ -135,14 +169,22 @@ public class intersectionScript : MonoBehaviour
 
         if (radius == (circCenter - center).magnitude)
         {
+ //TODO @Rene calculate figure parameters
+
         //comment here to explain what this case is geometerically
             Vector3 pointPos = new Vector3();
+//TODO @Camden Render Figure
+            
         }
         else if (radius > (circCenter - center).magnitude)
         {
+ //TODO @Rene calculate figure parameters
+
          //comment here to explain what this case is geometerically
             Vector3 planePos = new Vector3();
             //in this case, we also want a radius and a center of the circle.
+ //TODO @Camden Render Figure
+           
         }
         //what if radius < 9circCenter-center).magnitude?
 
@@ -150,8 +192,10 @@ public class intersectionScript : MonoBehaviour
 
     public void crossSectTorus(float innerRadius, float outerRadius, Vector3 center, Vector3 height, Vector3 plane1Norm, Vector3 plane2Norm)
     {
+ //TODO @Rene calculate figure parameters
         
-        
+ //TODO @Camden Render Figure
+       
     }
 
 }
