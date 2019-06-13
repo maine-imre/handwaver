3a941856e51d4c648146894247b4f7ef0bde0344
Camden Bock <camden.bock@maine.edu>
2019-06-13 13:08:58 -0400

    Update BodyComponent.cs

--- Assets/Scripts/EmbodiedInput/BodyComponent.cs
+++ Assets/Scripts/EmbodiedInput/BodyComponent.cs
@@ -11,22 +11,32 @@ namespace IMRE.EmbodiedUserInput
     [System.Serializable]
     public struct BodyComponent
     {
+        private float3x3 data;
         /// <summary>
         /// The position of a given joint
         /// </summary>
-        public float3 Position;
+        public float3 Position{
+            get {return data.c0;}
+            set {data.c0 = value;}
+        }
 
         /// <summary>
         /// The direction of a joint.
         /// Unless otherwise noted, this is a assumeed to be the radial direction
         /// of the previous connecting bone
         /// </summary>
-        public float3 Direction;
+        public float3 Direction{
+            get {return data.c1;}
+            set {data.c1 = value;}
+        }
 
         /// <summary>
         /// The average velocity of the component over the last 10 frames.
         /// This is done on seperate calculation, and it is not needed to be updated outside of <see cref="BodyInputDataSystem"/>
         /// </summary>
-        public float3 Velocity;
+        public float3 Velocity{
+            get {return data.c2;}
+            set {data.c2 = value;}
+        }
     }
 }
