2abf94d3c8e6f5367ce1e2a85fe457d29a47f2df
Joey Haney <joseph.e.haney@maine.edu>
2019-06-25 14:09:48 -0400

    undeleting files we want which were deleted in previous commit from this pc

---
+++ Assets/Scripts/EmbodiedInput/EmbodiedUserInputAction.cs.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using IMRE.Math;
+using Unity.Burst;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEditor;
+
+namespace IMRE.EmbodiedUserInput
+{
+    
+    /// <summary>
+    /// This is an example class that uses the embodied input classifier in a Job Component System
+    /// </summary>
+    public class EmbodiedUserInputAction : JobComponentSystem
+    {
+        public enum ActionType
+        {
+            debug, 
+        };
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public struct ActionData : IComponentData
+        {
+            //Todo the data that is needed to do the job
+            public int priority;
+            public ActionType actionType;
+            public bool isActive;
+            public bool activatable;
+            
+            /// <summary>
+            /// The GGB command string to send to the server
+            /// </summary>
+            public string command;
+        }
+
+        /// <summary>
+        /// A thin layer of general abstraction for one-handed and two-handed gestures.
+        /// Inspired by LeapPaint https://github.com/leapmotion/Paint
+        /// </summary>
+        [BurstCompile]
+        public struct EmbodiedUserInputClassifierJob : IJobForEach<ActionData, EmbodiedUserInputClassifier.EmbodiedClassifier>
+        {
+            
+            public void Execute(ref ActionData actionData, ref EmbodiedUserInputClassifier.EmbodiedClassifier embodiedClassifier)
+            {
+                if (embodiedClassifier.isEligible)
+                {
+                    actionData.activatable = true;
+                    if (embodiedClassifier.wasActivated)
+                    {
+                        //was turned on last cycle
+                        switch (actionData.actionType)  
+                        {
+                            case ActionType.debug :
+                                actionData.command = "start";
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        //continuing to run
+                        switch (actionData.actionType)  
+                        {
+                            case ActionType.debug :
+                                actionData.command = "on";
+                                break;
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    actionData.isActive = false;
+                    actionData.activatable = false;
+                    if (embodiedClassifier.wasCancelled)
+                    {
+                        //Todo  provide feedback gesutre is 
+                        switch (actionData.actionType)  
+                        {
+                            case ActionType.debug :
+                                actionData.command = "cancelled";
+                                break;
+                        }
+                    }else if (embodiedClassifier.wasFinished)
+                    {
+                        //todo provide feedback gesture is finished
+                        switch (actionData.actionType)  
+                        {
+                            case ActionType.debug :
+                                actionData.command = "finished";
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        //todo remove feedback
+                        switch (actionData.actionType)  
+                        {
+                            case ActionType.debug :
+                                actionData.command = "off";
+                                break;
+                        }
+                    }
+                }
+                
+                //todo turn on and off feedback
+            }
+        }
+        // OnUpdate runs on the main thread.
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            var job = new EmbodiedUserInputClassifierJob();
+            
+            return job.Schedule(this, inputDependencies);
+        }
+
+    }
+}
---
+++ Assets/Scripts/EmbodiedInput/EmbodiedUserInputActionAuthoring.cs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Instrumentation;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace IMRE.EmbodiedUserInput
+{
+    public class EmbodiedUserInputActionAuthoring : MonoBehaviour
+    {
+        public Entity actionPrefab;
+        
+        internal EntityManager manager;
+        internal static NativeArray<Entity> entities;
+        
+        
+        void Start()
+        {
+            manager = World.Active.GetOrCreateSystem<EntityManager>();
+            addClassifierBatch();
+        }
+
+        /// <summary>
+        /// Adds one of each type of classifier to the job list
+        /// </summary>
+        void addClassifierBatch()
+        {
+            int amount = EmbodiedUserInputClassifierAuthoring.entities.Length;
+            entities = new NativeArray<Entity>(amount, Allocator.Temp);
+            manager.Instantiate(actionPrefab, entities);
+            
+            //SETUP DEBUG ENTITIES;
+            for (int i = 0; i < amount; i++)
+            {
+                manager.SetComponentData(entities[i], new EmbodiedUserInputAction.ActionData
+                {
+                    priority = 1000,
+                    actionType = EmbodiedUserInputAction.ActionType.debug
+                    //todo associate classifier
+                });
+                manager.SetComponentData(entities[0], EmbodiedUserInputClassifierAuthoring.manager.GetComponentData<EmbodiedUserInputClassifier.EmbodiedClassifier>(EmbodiedUserInputClassifierAuthoring.entities[i]));
+            }
+        }
+
+        private void LateUpdate()
+        {
+            //READ DEBUG ENTITIES
+            int amount = EmbodiedUserInputClassifierAuthoring.entities.Length;
+            for (int i = 0; i < amount; i++)
+            {
+                Debug.Log(
+                    manager.GetComponentData<EmbodiedUserInputClassifier.EmbodiedClassifier>(
+                        EmbodiedUserInputClassifierAuthoring.entities[i]).type + " " + 
+                        manager.GetComponentData<EmbodiedUserInputAction.ActionData>(entities[i]).command);
+            }
+            
+        }
+    }
+}
