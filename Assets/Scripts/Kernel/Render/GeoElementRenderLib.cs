6f0e461f5d2b5f2b407d7c44e049445360bfc51f
Nathan Gazey <ngazey60@maine.edu>
2019-07-08 11:55:29 -0400

    added kernal rendering stub functionality

---
+++ Assets/Scripts/Kernel/Render/GeoElementRenderLib.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace IMRE.HandWaver.Rendering
+{
+    /// <summary>
+    /// Contains functional code that would render out a given element based on passed in data.
+    /// </summary>
+    public class GeoElementRenderLib
+    {
+        // TODO: Group by element type.
+        //    -- Point
+        //    -- Path
+        //    -- Surface
+        //    -- Region
+        //    -- NonGeometeric
+        // TODO: Determine input vars.
+        // TODO: Implement functionality for base version of HW Rendering.
+        
+        public bool Line(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Point(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Segment(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+        
+        #region Unimplemented
+        public bool Angle(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Axis(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Conic(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConicPart(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CurveCartesian(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Function(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FunctionND(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Interval(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Locus(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LocusND(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Number(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Poly(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PolyLine(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Ray(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SurfaceFinite(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Symbolic(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Vector2D(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Vector3D(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Text(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Turtle(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Video(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Image(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Slider(/*Data needed to render*/)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
