e1ab1014eb2d9013f8bcaae067755edf0608c5b6
Camden Bock <camden.bock@maine.edu>
2019-07-22 22:02:10 -0400

    add abstractions

---
+++ Assets/Scripts/SceneSpecific/ScaleDimensionStudy/Nets/net2D.cs
@@ -0,0 +1,31 @@
+﻿namespace IMRE.HandWaver.ScaleStudy
+{
+    public class net2D : UnityEngine.MonoBehaviour, ISliderInput
+    {
+        private float _percentFolded;
+
+        public System.Collections.Generic.List<UnityEngine.GameObject> foldPoints =
+            new System.Collections.Generic.List<UnityEngine.GameObject>();
+
+        public bool sliderOverride;
+
+        public float PercentFolded
+        {
+            get => _percentFolded;
+
+            set
+            {
+                //set vertices using vert function
+                _percentFolded = value;
+                GetComponent<UnityEngine.LineRenderer>().SetPositions(verts(_percentFolded));
+            }
+        }
+
+        public float slider
+        {
+            set => PercentFolded = !sliderOverride ? value : 1f;
+        }
+
+	public abstract UnityEngine.Vector3[] verts(float percentFolded);
+    }
+}
