06f69a33468b1dd5dc45043a2fbd35c79f815f3c
Joey Haney <joseph.e.haney@maine.edu>
2019-08-22 18:20:12 -0400

    Began work with the Handwaver scene. This is in an attempt to test the GGB connection with live usage while in a scene. #14 #9

---
+++ Assets/Scripts/SceneSpecific/HandWaver/ActionSystem/GeoElementProximityLib.cs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IMRE.HandWaver.Kernel.Geos;
+using Unity.Mathematics;
+using UnityEngine;
+
+public static class GeoElementProximityLib
+{
+
+public static float distToGeoElement(GeoElement Geo, float3 location)
+{
+    return 0f;
+}
+
+public static float3 closestPointOnSurface(GeoElement Geo, float3 location)
+{
+    return float3.zero;
+}
+
+}
---
+++ Assets/Scripts/SceneSpecific/HandWaver/ActionSystem/MoveObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IMRE.EmbodiedUserInput;
+using IMRE.HandWaver.ActionSystem;
+using IMRE.HandWaver.Kernel.Geos;
+using UnityEngine;
+
+namespace IMRE.HandWaver.ActionSystem
+{
+    
+    public class MoveObject : GeoElementFunctions
+    {
+        public override void GeoElementFunction(GeoElement geo, EmbodiedClassifier classifier)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
