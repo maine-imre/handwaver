33ec714712235e6a0b4c32faea2402f291732b2e
Joey Haney <joseph.e.haney@maine.edu>
2019-08-23 17:04:08 -0400

    Beginning of ggb Handwaver functionality

---
+++ Assets/Scripts/Kernel/ActionSystem/MakePoint.cs
@@ -0,0 +1,16 @@
+﻿using IMRE.EmbodiedUserInput;
+using IMRE.HandWaver.ActionSystem;
+using IMRE.HandWaver.Kernel.Geos;
+using IMRE.HandWaver.Kernel.GGBFunctions;
+
+namespace IMRE.HandWaver.Kernel.ActionSystem
+{
+    public class MakePoint : GeoElementFunction
+    {
+        public override void geoElementFunction(GeoElement geo, EmbodiedClassifier classifier)
+        {
+            HandWaverServerTransport.execCommand("Point(" + classifier.origin.ToString() + ")");
+        }
+
+    }
+}
