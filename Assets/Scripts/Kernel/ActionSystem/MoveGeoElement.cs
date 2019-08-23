eceeaf84464e27c4ecf8ff46d6a93eb55b410575
Nathan Gazey <ngazey60@maine.edu>
2019-08-23 10:59:14 -0400

    added missing abstract method

--- Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
+++ Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
@@ -1,5 +1,11 @@
-﻿using IMRE.HandWaver.ActionSystem;
+﻿using IMRE.EmbodiedUserInput;
+using IMRE.HandWaver.ActionSystem;
+using IMRE.HandWaver.Kernel.Geos;
 
 public class MoveGeoElement : GeoElementFunction
 {
+    public override void geoElementFunction(GeoElement geo, EmbodiedClassifier classifier)
+    {
+        throw new System.NotImplementedException();
+    }
 }
