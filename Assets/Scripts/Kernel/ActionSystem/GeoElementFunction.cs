82ae8eda5183e8340bce02a1e396756f1b6ae97a
Nathan Gazey <ngazey60@maine.edu>
2019-08-23 10:25:48 -0400

    geoelement action system added

---
+++ Assets/Scripts/Kernel/ActionSystem/GeoElementFunction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IMRE.EmbodiedUserInput;
+using IMRE.HandWaver.Space;
+using IMRE.Math;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace IMRE.HandWaver.ActionSystem
+{
+    public class GeoElementFunction : EmbodiedAction
+    {
+        public float tolerance = .05f;
+        public float angleTolerance = 15f;
+        private float desiredAngle = 0f;
+
+        public override void checkClassifier(EmbodiedClassifier classifier)
+        {
+            float bestDist = tolerance;
+            GeoElement geo = new GeoElement();
+            //find closest point
+            for (int i = 0; i < GeoElementDatabase.GeoElements.Values.Count; i++)
+            {
+                float3 closestPoint = GeoElementProximityLib.closestPointOnSurface(GeoElementDatabase.GeoElements.Values[i]);
+                if ((Operations.magnitude(classifier.origin - closestPoint) < bestDist) &&
+                    (Operations.Angle(classifier.direction,classifier.origin - closestPoint) < angleTolerance))
+                {
+                    geo = GeoElementDatabase.GeoElements.Values[i];
+                    bestDist = Operations.magnitude(classifier.origin - closestPoint);
+                }
+            }
+
+            if (!geo.Equals(default(GeoElement)))
+            {
+                geoElementFunction(geo, classifier);
+            }
+        }
+
+        public override void endAction(EmbodiedClassifier classifier)
+        {
+            return;
+        }
+
+        public abstract void geoElementFunction(GeoElement geo, EmbodiedClassifier classifier);
+    }
+}
---
+++ Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IMRE.HandWaver.ActionSystem;
+using UnityEngine;
+
+public class MoveGeoElement : GeoElementFunction
+{
+    
+}
