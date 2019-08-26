60bc7ef5bd3374a92bcb43fe56a8a8e104a407df
Joey Haney <joseph.e.haney@maine.edu>
2019-08-26 16:06:00 -0400

    GGB make and move pin functionality pre test
    remember to discuss moving other things beyond points

--- Assets/Scripts/Kernel/ActionSystem/MakePoint.cs
+++ Assets/Scripts/Kernel/ActionSystem/MakePoint.cs
@@ -3,15 +3,32 @@ using IMRE.EmbodiedUserInput;
 using IMRE.HandWaver.ActionSystem;
 using IMRE.HandWaver.Kernel.Geos;
 using IMRE.HandWaver.Kernel.GGBFunctions;
+using UnityEngine;
 
 namespace IMRE.HandWaver.Kernel.ActionSystem
 {
     public class MakePoint : GeoElementFunction
     {
+
+        /// <summary>
+        /// Cooldown is used here to limit the amount of times the classifier's function is executed
+        /// </summary>
+        public float cooldown = 2f;
+        
+        /// <summary>
+        /// Start time is used to note when the classifier's function was last activated
+        /// </summary>
+        private float _startTime = 0f;
         public override void geoElementFunction(GeoElement geo, EmbodiedClassifier classifier)
         {
-            Geometry.Point("" + classifier.origin.ToString());
-            //HandWaverServerTransport.execCommand("Point(" + classifier.origin.ToString() + ")");
+            //Check to see that the cooldown is done on the gesture
+            if (Time.time > (_startTime + cooldown))
+            {
+                //set the time for when the function is called
+                _startTime = Time.time;
+                
+                Geometry.Point(Geometry.Float3Value(classifier.origin));
+            }
         }
     }
 }
--- Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
+++ Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
@@ -1,6 +1,9 @@
-﻿using IMRE.EmbodiedUserInput;
+﻿using System;
+using IMRE.EmbodiedUserInput;
 using IMRE.HandWaver.ActionSystem;
 using IMRE.HandWaver.Kernel.Geos;
+using IMRE.HandWaver.Kernel.GGBFunctions;
+using UnityEngine;
 
 namespace IMRE.HandWaver.Kernel.ActionSystem
 {
@@ -8,7 +11,24 @@ namespace IMRE.HandWaver.Kernel.ActionSystem
     {
         public override void geoElementFunction(GeoElement geo, EmbodiedClassifier classifier)
         {
-            throw new System.NotImplementedException();
+                switch (geo.Type)
+                {
+                    case ElementType.point:
+                        Geometry.Point(Geometry.Float3Value(classifier.origin));
+                        break;
+                    case ElementType.line:
+                        //design discussion needed here
+                        //Geometry.Line(Geometry.Float3Value(classifier.origin), );
+                        break;
+                    case ElementType.plane:
+                        break;
+                    case ElementType.sphere:
+                        break;
+                    case ElementType.circle:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
         }
     }
 }
