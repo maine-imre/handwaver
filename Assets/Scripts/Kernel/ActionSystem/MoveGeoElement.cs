4977a96cfd4f288f8647d1d9720d620d5c6bdd38
Camden Bock <camden.bock@maine.edu>
2019-09-17 10:33:04 -0400

    Update MoveGeoElement.cs

--- Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
+++ Assets/Scripts/Kernel/ActionSystem/MoveGeoElement.cs
@@ -11,26 +11,48 @@ namespace IMRE.HandWaver.Kernel.ActionSystem
     {
         public override void geoElementFunction(GeoElement geo, EmbodiedClassifier classifier)
         {
+        //TODO add rotations to this script.  As written, the script only handles translations.
                 switch (geo.Type)
                 {
+                    float3 diff = (classifier.origin-GeoElementProximityLib.ClosestPosition(geo, classifier.origin));
                     case ElementType.point:
-                        HandWaverServerTransport.execCommand(
-                            geo.ElementName.ToString() + " = (" + Geometry.Float3Value(classifier.origin) + ")");
+                    StartCoroutine(HandWaverServerTransport.execCommand(
+                    "geo.ElementName.ToString() + " = ("+classifier.origin.x+","+classifier.origin.y+","+classifier.origin.z+")"));
                         Debug.Log("I'm here");
                         break;
                     case ElementType.line:
-                        //design discussion needed here
-                        //Geometry.Line(Geometry.Float3Value(classifier.origin), );
+                        float3 pointA = GeoElementDataBase.GeoElements[geo.Deps[0]].F0 + diff;
+                        float3 pointB = GeoElementDataBase.GeoElements[geo.Deps[1]].F0 + diff;
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[0].ElementName.ToString() + " = ("+pointA.x+","+pointA.y+","+pointA.z+")"));
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[1].ElementName.ToString() + " = ("+pointB.x+","+pointB.y+","+pointB.z+")"));
                         break;
                     case ElementType.plane:
+                        float3 pointA = GeoElementDataBase.GeoElements[geo.Deps[0]].F0 + diff;
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[0].ElementName.ToString() + " = ("+pointA.x+","+pointA.y+","+pointA.z+")"));
                         break;
                     case ElementType.sphere:
+                        float3 pointA = GeoElementDataBase.GeoElements[geo.Deps[0]].F0 + diff;
+                        float3 pointB = GeoElementDataBase.GeoElements[geo.Deps[1]].F0 + diff;
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[0].ElementName.ToString() + " = ("+pointA.x+","+pointA.y+","+pointA.z+")"));
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[1].ElementName.ToString() + " = ("+pointB.x+","+pointB.y+","+pointB.z+")"));
                         break;
                     case ElementType.circle:
+                        float3 pointA = GeoElementDataBase.GeoElements[geo.Deps[0]].F0 + diff;
+                        float3 pointB = GeoElementDataBase.GeoElements[geo.Deps[1]].F0 + diff;
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[0].ElementName.ToString() + " = ("+pointA.x+","+pointA.y+","+pointA.z+")"));
+                        StartCoroutine(HandWaverServerTransport.execCommand(
+                        geo.Deps[1].ElementName.ToString() + " = ("+pointB.x+","+pointB.y+","+pointB.z+")"));
                         break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
+                    case defaut;
+                        debug.LogWarning("Type Not Supported");
+                        break;
+            }
         }
     }
 }
