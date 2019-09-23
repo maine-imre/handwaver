a251e4c0c0c7619a53b81e30ec9abf99e494c213
Rene Yost <rene.yost@maine.edu>
2019-09-23 16:42:48 -0400

    Have to rework cross section functions

--- Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/SquareCrossSection.cs
+++ Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/SquareCrossSection.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Unity.Mathematics;
 
-public class SquareCrossSection : MonoBehaviour
+namespace IMRE.HandWaver.ScaleStudy
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SquareCrossSection : UnityEngine.MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Function to render the intersection of a plane and a square
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="vertices"></param>
+        /// <param name="crossSectionRenderer"></param>
+        public void crossSectSquare(float3 height, float3 direction, Vector3[] vertices, LineRenderer crossSectionRenderer)
+        {
+            Vector3 topLeft = vertices[0];
+            Vector3 topRight = vertices[1];
+            Vector3 bottomRight = vertices[2];
+            Vector3 bottomLeft = vertices[3];
+
+            Vector3 segmentEndPoint0;
+            Vector3 segmentEndPoint1;
+
+            //TODO: Determine if we want to include points for end of segment
+            if (height == topLeft.y && height == topRight.y)
+            {
+                segmentEndPoint0 = new Vector3(topLeft.x, topLeft.y, topLeft.z);
+                segmentEndPoint1 = new Vector3(topRight.x, topRight.y, topRight.z);
+
+                crossSectionRenderer.enabled = true;
+                crossSectionRenderer.SetPosition(0, segmentEndPoint0);
+                crossSectionRenderer.SetPosition(1, segmentEndPoint1);
+            }
+            
+            else if (height > topLeft.y || height < bottomRight.y)
+            {
+                
+            }
+            
+            else if ((height < topLeft.y && height >= bottomLeft.y) || (height < topRight.y && height <= bottomLeft.y))
+            {
+                segmentEndPoint0 = new Vector3(0, topLeft.y - height, topLeft.z);
+                segmentEndPoint1 = new Vector3(0, topRight.y - height, topRight.z);
+
+                crossSectionRenderer.enabled = true;
+                crossSectionRenderer.SetPosition(0, segmentEndPoint0);
+                crossSectionRenderer.SetPosition(1, segmentEndPoint1);
+            }
+        }
     }
 }
+    
--- Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/TriangleCrossSection.cs
+++ Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/TriangleCrossSection.cs
@@ -5,42 +5,25 @@ namespace IMRE.HandWaver.ScaleStudy
 
 {
 
-    public class TriangleCrossSection : UnityEngine.MonoBehaviour//, IMRE.HandWaver.ScaleStudy.ISliderInput
+    public class TriangleCrossSection : UnityEngine.MonoBehaviour
     {
        /// <summary>
-       /// Function to render the intersetion of a plane and a triangle
+       /// Function to render the intersection of a plane and a triangle
        /// </summary>
        /// <param name="height"></param>
        /// <param name="vertices"></param>
        /// <param name="crossSectionRenderer"></param>
-        public void crossSectTri(float height, Vector3[] vertices, LineRenderer crossSectionRenderer)
-        {
-            Vector3 segmentEndPoint0, segmentEndPoint1;
-            Vector3 triangleTop = vertices[0];
-            Vector3 bottomRight = vertices[1];
-            Vector3 bottomLeft = vertices[2];
+        public void crossSectTri(float3 point, float3 direction, Vector3[] vertices, LineRenderer crossSectionRenderer)
+       {
 
-            //TODO: determine what we want to do with points
-            if (height == triangleTop.y)
-            {
-                segmentEndPoint0 = triangleTop;
-                crossSectionRenderer.enabled = true;
-                crossSectionRenderer.SetPosition(0, segmentEndPoint0);
-                crossSectionRenderer.SetPosition(1, segmentEndPoint0);
-            }
+           Vector3 a = vertices[0];
+           Vector3 b = vertices[1];
+           Vector3 c = vertices[2];
+           
+           Vector3 ab_hat = (b - a) / 
 
-            if (height < triangleTop.y)
-            {
-                segmentEndPoint0 = new Vector3(0, triangleTop.y - height, -(height / math.sqrt(3)));
-                segmentEndPoint1 = new Vector3(0, triangleTop.y - height, -(height / math.sqrt(3)));
-                crossSectionRenderer.enabled = true;
-                crossSectionRenderer.SetPosition(0, segmentEndPoint0);
-                crossSectionRenderer.SetPosition(1, segmentEndPoint1);
-            }
-                
-               
 
-        }
+       }
 
 
 
