951ec04ce4068ccda67df6a88d4cd213f857408e
Rene Yost <rene.yost@maine.edu>
2019-09-24 19:26:36 -0400

    Reworking cross-sections, intersection can result in more cases than I thought previously

--- Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/SquareCrossSection.cs
+++ Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/SquareCrossSection.cs
@@ -14,39 +14,9 @@ namespace IMRE.HandWaver.ScaleStudy
         /// <param name="crossSectionRenderer"></param>
         public void crossSectSquare(float3 height, float3 direction, Vector3[] vertices, LineRenderer crossSectionRenderer)
         {
-            Vector3 topLeft = vertices[0];
-            Vector3 topRight = vertices[1];
-            Vector3 bottomRight = vertices[2];
-            Vector3 bottomLeft = vertices[3];
-
-            Vector3 segmentEndPoint0;
-            Vector3 segmentEndPoint1;
-
-            //TODO: Determine if we want to include points for end of segment
-            if (height == topLeft.y && height == topRight.y)
-            {
-                segmentEndPoint0 = new Vector3(topLeft.x, topLeft.y, topLeft.z);
-                segmentEndPoint1 = new Vector3(topRight.x, topRight.y, topRight.z);
-
-                crossSectionRenderer.enabled = true;
-                crossSectionRenderer.SetPosition(0, segmentEndPoint0);
-                crossSectionRenderer.SetPosition(1, segmentEndPoint1);
-            }
             
-            else if (height > topLeft.y || height < bottomRight.y)
-            {
-                
-            }
             
-            else if ((height < topLeft.y && height >= bottomLeft.y) || (height < topRight.y && height <= bottomLeft.y))
-            {
-                segmentEndPoint0 = new Vector3(0, topLeft.y - height, topLeft.z);
-                segmentEndPoint1 = new Vector3(0, topRight.y - height, topRight.z);
-
-                crossSectionRenderer.enabled = true;
-                crossSectionRenderer.SetPosition(0, segmentEndPoint0);
-                crossSectionRenderer.SetPosition(1, segmentEndPoint1);
-            }
+            
         }
     }
 }
--- Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/TriangleCrossSection.cs
+++ Assets/Scripts/SceneSpecific/ScaleDimensionStudy/CrossSection/TriangleCrossSection.cs
@@ -16,13 +16,58 @@ namespace IMRE.HandWaver.ScaleStudy
         public void crossSectTri(float3 point, float3 direction, Vector3[] vertices, LineRenderer crossSectionRenderer)
        {
 
-           Vector3 a = vertices[0];
-           Vector3 b = vertices[1];
-           Vector3 c = vertices[2];
+           crossSectionRenderer = new LineRenderer();
            
-           Vector3 ab_hat = (b - a) / 
+           //Vertices are organized in clockwise manner
+           float3 a = vertices[0];
+           float3 b = vertices[1];
+           float3 c = vertices[2];
 
+           float3 segmentab = b - a;
+           float3 segmentac = c - a;
+           float3 segmentbc = c - b;
+           float3 intersectionLine = direction - point;
+           
+           float3 ac_hat = (c - a) / Vector3.Magnitude(c - a);
+           float3 ab_hat = (b - a) / Vector3.Magnitude(b - a);
+           float3 bc_hat = (c - b) / Vector3.Magnitude(c - b);
 
+           float3 ac_star = Vector3.Project(intersectionLine - segmentac, ac_hat) + new Vector3(a.x, a.y, a.z);
+           float3 ab_star = Vector3.Project(intersectionLine - segmentab, ab_hat) + new Vector3(a.x, a.y, a.z);
+           float3 bc_star = Vector3.Project(intersectionLine - segmentbc, bc_hat) + new Vector3(b.x, b.y, b.z);
+
+           //If plane does not hit triangle
+           if (Vector3.Magnitude(ac_star - a) > Vector3.Magnitude(c - a) ||
+               Vector3.Magnitude(ac_star - c) > Vector3.Magnitude(c - a))
+           {
+               Debug.Log("Plane does not intersect with triangle.");
+           }
+
+           //Point of intersection is a vertex (a or c)
+           else if (Vector3.Magnitude(ac_star - a) == 0 || Vector3.Magnitude(ac_star - c) == 0)
+           {
+               crossSectionRenderer.enabled = true;
+
+           }
+           //Point of intersection is a or b
+           else if (Vector3.Magnitude(ab_star - a) == 0 || Vector3.Magnitude(ab_star - b) == 0)
+           {
+               crossSectionRenderer.enabled = true;
+
+           }
+           
+           else if (Vector3.Magnitude(bc_star - b) == 0 || Vector3.Magnitude(bc_star - c) == 0)
+           {
+               crossSectionRenderer.enabled = true;
+               
+              
+           }
+           
+           //c_star is on segmentac
+           else
+           {
+               
+           }
        }
 
 
