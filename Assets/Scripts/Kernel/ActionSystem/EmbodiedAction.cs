ea900f4d20ef93929dedf13df958a152ba196d2b
Joey Haney <joseph.e.haney@maine.edu>
2019-11-07 13:37:55 -0500

    Update EmbodiedAction.cs

--- Assets/Scripts/Kernel/ActionSystem/EmbodiedAction.cs
+++ Assets/Scripts/Kernel/ActionSystem/EmbodiedAction.cs
@@ -6,13 +6,27 @@ namespace IMRE.EmbodiedUserInput
     public abstract class EmbodiedAction : MonoBehaviour
     {
         public classifierType type;
+        
+        private IEnumerator coroutine;
+        public int frequency = 30;
+        private float deltaTime => 1f/((float) frequency);
+        
+        private void Start(){
+            coroutine = RunClassifiers(deltaTime);
+            StartCoroutine(coroutine);
+        }
 
-        private void Update()
+
+        private IEnumerator RunClassifiers(float deltaTime)
         {
+            while(true)
+            {
+            yield return new WaitForSeconds(waitTime);
             EmbodiedUserInputClassifierAuthoring.classifiers.ToList().Where(c => c.type == type && c.isEligible)
                 .ToList().ForEach(checkClassifier);
             EmbodiedUserInputClassifierAuthoring.classifiers.ToList().Where(c => c.type == type && c.wasCancelled)
                 .ToList().ForEach(endAction);
+           }
         }
 
         //Note that this doesn't imply that there is an actionable action, only an eligible classifier
